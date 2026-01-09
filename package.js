be2baab7abd1e134338ce7ab9b6f7300ff884c12
denavigator <asumanlukomwa001@gmail.com>
2026-01-09 08:59:17 +0300

    Initialize package.json for LukTech MD Bot

---
+++ package.js
@@ -0,0 +1,32 @@
+{
+  "name": "luktech-md",
+  "version": "3.2.0",
+  "description": "LukTech MD WhatsApp Bot with Panel Deploy",
+  "main": "index.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node index.js",
+    "panel": "node panel/server.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.8",
+    "pino": "^8.17.0",
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
